8d310b040905e343e531a2d3855c4dd0962dbd57
lpage30 <lawrence.page@live.com>
2025-01-20 08:30:40 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ script_path=$( cd -- "$(dirname "${BASH_SOURCE[0]}x")" >/dev/null 2>&1 ; pwd -P 
 rm -rf ${script_path}/build/
 rm -rf ${script_path}/dist/
 rm -rf ${script_path}/imagecloud.egg-info/
+rm -rf ${script_path}/imagecloud/__pycache__/
 rm -f ${script_path}/imagecloud/*.c
 rm -f ${script_path}/imagecloud/*.so
 rm -f ${script_path}/*.so
